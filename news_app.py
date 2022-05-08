37dd5ad5b32c75cc3a8386a4baac4b397c8ed424
matsudasatsuki <yk.xxx.org@gmail.com>
2022-05-08 13:43:10 +0900

    add scraping

--- news_app.py
+++ news_app.py
@@ -2,9 +2,24 @@ api_key = 'a64fadec7ec7420cb83b631a2f1b203a'
 from joblib import PrintTime
 import requests
 import pandas as pd
+import streamlit as st
+import numpy as np
+import os
+import urllib.request
+import json
+import configparser
+import codecs
+import requests
+from bs4 import BeautifulSoup
+import re
+
+st.title('News summarize')
+form = st.form(key='my_form')
+input = form.text_area(label='write here',max_chars=140,placeholder='ここに入力')
+submit_button = form.form_submit_button(label='Submit')
+
 
 headers = {'X-Api-Key': 'a64fadec7ec7420cb83b631a2f1b203a'}
-
 url = 'https://newsapi.org/v2/top-headlines'
 params = {
     'category' : 'business',
@@ -16,8 +31,12 @@ response = requests.get(url, headers=headers, params=params)
 if response.ok:
     data = response.json()
     df = pd.DataFrame(data['articles'])
-    print('totalResults:', data['totalResults'])
+    #print('totalResults:', data['totalResults'])
 
 #print(df[['publishedAt','title','url']])
-a = df['url'][0]
+a = df['url'][1]
 print(a)
+vgm_url = a
+html_text = requests.get(vgm_url).text
+soup = BeautifulSoup(html_text, 'html.parser',from_encoding='utf-8')
+print(soup.title.string)
